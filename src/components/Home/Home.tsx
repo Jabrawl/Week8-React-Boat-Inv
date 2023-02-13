c26bd7c48d76c7174b090b5f29ec7ddda883e4d3
Jabrawl <jamesstibral@gmail.com>
2023-02-13 15:09:52 -0600

    Boat Inventory

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,98 @@
+import React from 'react';
+import { styled } from '@mui/system';
+import { Button } from '@mui/material';
+import { Link } from 'react-router-dom';
+import boat_image from '../../assets/images/boat_image.avif'
+
+interface Props{
+    title: string;
+}
+
+// Create styled Elements with styled from mui/system
+const Root = styled('div')({
+    padding: 0,
+    margin: 0
+})
+
+const NavbarContainer = styled('div')({
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+})
+
+const Logo = styled('h1')({
+    margin: '0 0 0 0.45em'
+})
+
+const LogoA = styled('a')({
+    color: 'rgb(28, 24, 22)',
+    listStyle: 'none',
+    textTransform: 'uppercase',
+    textDecoration: 'none'
+})
+
+const LogoNavigation = styled('ul')({
+    listStyle: 'none',
+    textTransform: 'uppercase',
+    textDecoration: 'none',
+    display: 'flex'
+})
+
+const NavA = styled(Link)({
+    display: 'block',
+    padding: '1em',
+    color: 'black'
+})
+
+const Main = styled('main')({
+    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${boat_image});`,
+    width: '100%',
+    height: '100%',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    position: 'absolute'
+})
+
+const MainText = styled('div')({
+    textAlign: 'center',
+    position: 'relative',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    color: 'white'
+})
+
+
+export const Home = (props:Props) => {
+
+
+    return (
+        <Root>
+            <NavbarContainer>
+                <Logo>
+                    <LogoA href='#'>Bob's Boats</LogoA>
+                </Logo>
+                <LogoNavigation>
+                    <li>
+                        <NavA to="/">Home</NavA>
+                    </li>
+                    <li>
+                        <NavA to="/dashboard">Dashboard</NavA>
+                    </li>
+                    <li>
+                        <NavA to="/signin">Sign In</NavA>
+                    </li>
+                </LogoNavigation>
+            </NavbarContainer>
+            <Main>
+                <MainText>
+                    <h1>{props.title}</h1>
+                    <p>Boats are fly.</p>
+                    <Button color='primary' variant='contained' component={Link} to='/dashboard'>See the Boats</Button>
+                </MainText>
+            </Main>
+        </Root>
+    )
+}
+
---
+++ src/components/SignIn/SignIn.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const SignIn = () => {
+    return (
+        <div>
+            <h1>Sign In Here</h1>
+        </div>
+    )
+}
